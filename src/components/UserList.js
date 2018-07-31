a3a32ec8f010feec742a466ec6cb5d2232c8820b
benjicwood <bencharlwood@gmail.com>
2018-07-31 19:35:02 +0100

    show default roles instead of id number

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react'
 
 export default class UserList extends Component {
     renderList() {
-
-        return this.props.users.map((user) => {
-            return (
-                <tr key={user.id} className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-                    <td>{user.first_name}</td>
-                    <td>{user.last_name}</td>
-                    <td>{user.roles}</td>
-
-                </tr>
-            )
-
-        })
-    }
+        return this.props.users.map(user => {
+          const userRole = this.props.roles.find(role => user.default_role === role.id)
+        //  const userRoles = this.props.roles.find(role => user.roles === role.id)
+          const userRoleName = userRole ? userRole.name : "TBC"
+          return (
+            <tr
+              key={user.id}
+              className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+            >
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{userRoleName}</td>
+            </tr>
+          );
+        });
+      }
 
     render() {
-        const { users } = this.props;
+        const { users } = this.props
 
         return (
             <div>
