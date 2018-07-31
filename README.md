5f92f9ae3f9010bd1b0505e9a0f4bf9d20ff230f
benjicwood <bencharlwood@gmail.com>
2018-07-31 22:30:49 +0100

    tidy up some code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # users-and-roles
 Users and Roles
+
+# to run
+Git Clone https://github.com/benjicwood/users-and-roles
+npm Install
+npm start
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,24 +5,24 @@ import sortNames from '../functions/sort'
 export default class UserList extends Component {
     renderList() {
         return this.props.users.sort(sortNames).map(user => {
-          const userRole = this.props.roles.find(role => user.default_role === role.id)
-        //  const userRoles = this.props.roles.find(role => user.roles === role.id)
+            const userRole = this.props.roles.find(role => user.default_role === role.id)
 
-          const userRoleName = userRole ? userRole.name : "TBC"
-          const userRoleColour = userRole ? userRole.colour : "808080"
-          return (
-            <tr
-              key={user.id}
-              className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-              style={{color: '#' + userRoleColour}}
-            >
-              <td>{user.first_name}</td>
-              <td>{user.last_name}</td>
-              <td>{userRoleName}</td>
-            </tr>
-          );
+            const userRoleName = userRole ? userRole.name : "TBC"
+            const userRoleColour = userRole ? userRole.colour : "808080"
+
+            return (
+                <tr
+                    key={user.id}
+                    className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+                    style={{ color: '#' + userRoleColour }}
+                >
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{userRoleName}</td>
+                </tr>
+            );
         });
-      }
+    }
 
     render() {
         const { users } = this.props
