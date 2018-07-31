b9a35d6f4d87bdf2da0cb6fd2c906452457bad86
benjicwood <bencharlwood@gmail.com>
2018-07-31 19:51:31 +0100

    set line colour by role colour in data

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,11 +5,16 @@ export default class UserList extends Component {
         return this.props.users.map(user => {
           const userRole = this.props.roles.find(role => user.default_role === role.id)
         //  const userRoles = this.props.roles.find(role => user.roles === role.id)
+     //   console.log(userRole)
+    //    const rowColour = this.props.roles.find(role => userRole === role.id)
           const userRoleName = userRole ? userRole.name : "TBC"
+          const userRoleColour = userRole ? userRole.colour : "black"
+console.log(userRoleColour)
           return (
             <tr
               key={user.id}
               className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+              style={{color: '#' + userRoleColour}}
             >
               <td>{user.first_name}</td>
               <td>{user.last_name}</td>
